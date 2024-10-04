86abb65d6d8f24a03f4c891e74e31ccc3695bb16
Rekam <wood54sola@gmail.com>
2024-10-04 21:40:41 +0800

    practical ex2

--- src/views/PracticalExamples.vue
+++ src/views/PracticalExamples.vue
@@ -1,25 +1,13 @@
 <script setup>
-import Ex1HelloWorld from '../components/BasicExamples/Ex1HelloWorld.vue';
-import Ex2HandlingInput from '../components/BasicExamples/Ex2HandlingInput.vue';
-import Ex3AttributeBindings from '../components/BasicExamples/Ex3AttributeBindings.vue';
-import Ex4ConditionalAndLoops from '../components/BasicExamples/Ex4ConditionalAndLoops.vue';
-import Ex5FormBinding from '../components/BasicExamples/Ex5FormBinding.vue';
-import Ex6SimpleComponent from '../components/BasicExamples/Ex6SimpleComponent.vue';
+import Ex1Markdown from '@/components/PracticalExamples/Ex1Markdown.vue';
+import Ex2FetchingData from '@/components/PracticalExamples/Ex2FetchingData.vue';
 </script>
 
 <template>
     <div>
-        <h1>Basic Examples</h1>
-        <Ex1HelloWorld />
+        <h1>Practical Examples</h1>
+        <Ex1Markdown />
         <hr />
-        <Ex2HandlingInput />
-        <hr />
-        <Ex3AttributeBindings />
-        <hr />
-        <Ex4ConditionalAndLoops />
-        <hr />
-        <Ex5FormBinding />
-        <hr />
-        <Ex6SimpleComponent />
+        <Ex2FetchingData />
     </div>
 </template>

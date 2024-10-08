6820cb77f64f88271949beef614fd119992537e1
Rekam <wood54sola@gmail.com>
2024-10-08 23:47:03 +0800

    ex4 & ex5

---
+++ src/components/PracticalExamples/Ball.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+            <circle class="ball" cx="100" cy="100" r="20" fill="red" />
+        </svg>
+    </div>
+</template>
+
+<style>
+#app {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    background-color: #f0f0f0;
+}
+
+.ball {
+    animation: bounce 0.5s ease-in-out infinite alternate;
+}
+
+@keyframes bounce {
+    0% {
+        cy: 180;
+    }
+
+    100% {
+        cy: 100;
+    }
+}
+</style>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
--- src/views/PracticalExamples.vue
+++ src/views/PracticalExamples.vue
@@ -2,6 +2,9 @@
 import Ex1Markdown from '@/components/PracticalExamples/Ex1Markdown.vue';
 import Ex2FetchingData from '@/components/PracticalExamples/Ex2FetchingData.vue';
 import Ex3Grid from '@/components/PracticalExamples/Ex3Grid.vue';
+import Ex4Tree from '@/components/PracticalExamples/Ex4Tree.vue';
+// import Ball from '@/components/PracticalExamples/Ball.vue';
+import Ex5SVG from '@/components/PracticalExamples/Ex5SVG.vue';
 </script>
 
 <template>
@@ -12,6 +15,13 @@ import Ex3Grid from '@/components/PracticalExamples/Ex3Grid.vue';
         <Ex2FetchingData />
         <hr />
         <Ex3Grid />
+        <hr />
+        <Ex4Tree />
+        <hr />
+        <!-- <Ball /> -->
+        <Ex5SVG />
+        <hr />
+
 
     </div>
 </template>

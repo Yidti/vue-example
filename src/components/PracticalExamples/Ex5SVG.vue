0c9da68f42deefd4d17f87f2f4fb6dfa692a7be0
Rekam <wood54sola@gmail.com>
2024-10-09 20:32:48 +0800

    fix css

--- src/components/PracticalExamples/Ex5SVG.vue
+++ src/components/PracticalExamples/Ex5SVG.vue
@@ -37,33 +37,48 @@ function remove(stat) {
 
 <template>
     <div class="container">
-        <p>Example 5:</p>
-        <svg width="200" height="200">
-            <PolyGraph :stats="stats"></PolyGraph>
-        </svg>
+        <div class="column">
+            <p>Example 5:</p>
+            <svg width="200" height="200">
+                <PolyGraph :stats="stats"></PolyGraph>
+            </svg>
 
-        <!-- controls -->
-        <div v-for="stat in stats">
-            <label>{{ stat.label }}</label>
-            <input type="range" v-model="stat.value" min="0" max="100">
-            <span>{{ stat.value }}</span>
-            <button @click="remove(stat)" class="remove">X</button>
+            <!-- controls -->
+            <div v-for="stat in stats">
+                <label>{{ stat.label }}</label>
+                <input type="range" v-model="stat.value" min="0" max="100">
+                <span>{{ stat.value }}</span>
+                <button @click="remove(stat)" class="remove">X</button>
+            </div>
+
+            <form id="add">
+                <input name="newlabel" v-model="newLabel">
+                <button @click="add">Add a Stat</button>
+            </form>
         </div>
 
-        <form id="add">
-            <input name="newlabel" v-model="newLabel">
-            <button @click="add">Add a Stat</button>
-        </form>
-
-        <pre id="raw">{{ stats }}</pre>
+        <div class="column">
+            <pre id="raw">{{ stats }}</pre>
+        </div>
     </div>
 
 </template>
 
 <style scoped>
 .container {
-    position: relative;
-    /* 設置父元素為相對定位 */
+    display: flex;
+    /* 設置容器為彈性盒 */
+    justify-content: space-between;
+    /* 在兩列之間分配空間 */
+    padding: 10px;
+    /* 加入內邊距 */
+}
+
+.column {
+    flex: 1;
+    /* 使每一列都可以佔據相同的空間 */
+    margin: 10px;
+    /* 每列添加間隙 */
 }
 
 polygon {
@@ -88,8 +103,27 @@ label {
 }
 
 #raw {
-    position: absolute;
-    top: 0;
-    left: 300px;
+
+    margin-left: auto;
+    /* 自動邊距將其推到右邊 */
+    margin-top: 10px;
+    /* 上邊距 */
+    max-width: 300px;
+    /* 限制最大寬度 */
+}
+
+pre {
+    max-width: 100%;
+    /* 限制最大寬度 */
+    overflow: auto;
+    /* 超出時顯示滾動條 */
+    margin: 10px 0;
+    /* 調整上下邊距 */
+    padding: 10px;
+    /* 增加內邊距 */
+    background-color: #f9f9f9;
+    /* 增加背景色 */
+    border: 1px solid #ccc;
+    /* 增加邊框 */
 }
 </style>
